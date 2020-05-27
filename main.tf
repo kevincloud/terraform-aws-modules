8cf950d204ddc41cde5ea55a12c9a2b888b50933
Kevin Cochran <kcochran@hashicorp.com>
2020-05-27 16:56:44 -0400

    add vpc module

--- main.tf
+++ main.tf
@@ -4,16 +4,17 @@ provider "aws" {
     region = var.aws_region
 }
 
-resource "aws_vpc" "primary-vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
-}
+module "custom-vpc" {
+    source  = "app.terraform.io/kevindemos/custom-vpc/aws"
+    version = "1.0.0"
 
+    aws_region = var.aws_region
+}
 module "custom-sg" {
     source  = "app.terraform.io/kevindemos/custom-sg/aws"
     version = "1.0.0"
 
     description = "my moduled security group"
     identifier = "kevinc"
-    vpc_id = aws_vpc.primary-vpc.id
+    vpc_id = module.custom-vpc.id
 }
