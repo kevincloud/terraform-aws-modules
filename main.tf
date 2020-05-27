6f87e4c12f4b16fcc5570cf220bc5e0d1f5f0b51
Kevin Cochran <kcochran@hashicorp.com>
2020-05-27 19:24:23 -0400

    update module version

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "dynamodb" {
 
 module "iam-role" {
     source  = "app.terraform.io/kevindemos/iam-role/aws"
-    version = "1.0.1"
+    version = "1.0.2"
 
     identifier = "kevinc"
     actions = [
