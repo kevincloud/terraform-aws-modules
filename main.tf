4d95f3a507c8acb9eb5a48759b9c2e285e61fcca
kevincloud <40193276+kevincloud@users.noreply.github.com>
2020-06-16 20:56:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "custom-sg" {
 
 module "dynamodb" {
     source  = "app.terraform.io/kevindemos/dynamodb/aws"
-    version = "1.0.0"
+    version = "1.0.1"
 
     identifier = var.identifier
     key_setup = {
