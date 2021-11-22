3e3f281e3b20202e10bcc9bdf72b0d0945ee0761
Kevin Cochran <kcochran@hashicorp.com>
2021-11-22 09:59:45 -0500

    remove tf variables

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
-    token = var.aws_session_token
     region = var.aws_region
 }
 
@@ -118,8 +115,4 @@ module "my-bucket" {
         Purpose = var.purpose
         TTL = var.ttl
     }
-}
-
-resource "aws_vpc" "primary-vpc" {
-    cidr_block = "10.10.0.0/16"
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,3 @@
-variable "aws_access_key" { }
-variable "aws_secret_key" { }
-variable "aws_session_token" { }
 variable "aws_region" { }
 variable "identifier" { }
 variable "key_pair" { }
